4e1754139ef7578de2c5baeade54dcc2054b0c1c
Jan <objectisnotdefined@gmail.com>
2025-05-30 00:16:19 +0800

    home: redirect to swap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { Button } from '@/components';
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.replace('/swap')
+  }, []);
+
   return (<div className="flex flex-col items-center justify-center min-h-[calc(100vh-154px)]">
     <h1 className="text-4xl font-bold">Aiigo DEX</h1>
   </div>);
